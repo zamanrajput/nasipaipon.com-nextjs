285dfd9988829b7feb632ea6b44168fa8ffd0247
Muhammad Zaman <zamanbhatti275@gmail.com>
2025-09-27 16:27:28 +0500

    sync

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+      unoptimized: true
+    }
+  }
+  
+  export default nextConfig
