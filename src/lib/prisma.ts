6d172da6652251d10fadee7be6f5c42b8f6eaf47
Gulfam-bhatti <arshadgulfam48@gmail.com>
2025-10-28 13:45:18 +0500

    authentication complete

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaClient } from "../../generated/prisma/client"
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
